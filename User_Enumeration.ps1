c40c0df982a631094d5cb4b0db6488c797636a51
duckingtoniii <sirduckingtontheiii@gmail.com>
2016-11-21 20:43:22 -0800

    Made output more readable

--- User_Enumeration.ps1
+++ User_Enumeration.ps1
@@ -1,11 +1,11 @@
-$String = net users /domain
+Write-Host -NoNewLine "Reading Users."
+$String = net users
 $String = $String[6..($String.length-3)]
 [System.Collections.ArrayList]$String = $String
 $count = 0
 $warning = ""
 $dupeWarn = ""
 $lastUser = ""
-Write-Host "Reading Users." -NoNewLine
 foreach ($line in $String) {
 	$line = $line.trim()
 	$line = $line -split '\s\s+'
@@ -20,14 +20,17 @@ foreach ($line in $String) {
 		Add-Content usernames.txt $user
 		$count = $count + 1
 		if ($count % 1000 -eq 0){
-			Write-Host "." -NoNewLine
+			Write-Host -NoNewLine "."
 		}
 	}
  }
-echo ""
-echo "Number of Users: $count"
-echo ""
-echo $warning
-echo ""
-echo $dupeWarn
-echo ""
+if ($warning) { 
+	Write-Output ""
+	Write-Host $warning -foregroundcolor "yellow"
+}
+if ($dupeWarn) { 
+	Write-Output ""
+	Write-Host $dupeWarn -foregroundcolor "yellow"
+}
+Write-Output ""
+Write-Host "Number of Users: $count" -foregroundcolor "green"
